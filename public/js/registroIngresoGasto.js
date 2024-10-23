eba3925443cb9b3470f97c6e464e69060acc323d
cafeconleche <edwin.mina@utp.ac.pa>
2024-10-22 23:59:02 -0500

    Create registroIngresoGasto.js
    si

---
+++ public/js/registroIngresoGasto.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM completamente cargado');
+    
+    var metodoEfectivo = document.getElementById('metodo-efectivo');
+    var metodoCheque = document.getElementById('metodo-cheque');
+    
+    console.log('Método Efectivo:', metodoEfectivo);
+    console.log('Método Cheque:', metodoCheque);
+    
+    if (metodoEfectivo && metodoCheque) {
+        metodoEfectivo.addEventListener('click', function () {
+            console.log('Método efectivo seleccionado');
+            mostrarFormulario('efectivo');
+        });
+
+        metodoCheque.addEventListener('click', function () {
+            console.log('Método cheque seleccionado');
+            mostrarFormulario('cheque');
+        });
+    } else {
+        console.error("Los elementos no se encontraron en el DOM.");
+    }
+
+    function mostrarFormulario(metodo) {
+        var chequeSection = document.getElementById('cheque-section');
+        if (metodo === 'efectivo') {
+            chequeSection.style.display = 'none';  // Ocultar campo de cheque si el método es efectivo
+        } else if (metodo === 'cheque') {
+            chequeSection.style.display = 'block'; // Mostrar campo de cheque si el método es cheque
+        }
+    }
+});
+
+
+
+ 
+  
+
+  
+  
+  function showMessage(message, type) {
+      const messageContainer = document.getElementById('responseMessage');
+      messageContainer.innerHTML = `<div class="alert alert-${type}" id="successMessage">${message}</div>`;
+  }
+
+
+
+  
+  
+  
