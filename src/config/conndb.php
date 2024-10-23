ba55a3dc64ddb9cc37a081073ee3a9add64ad5c9
ramxv <ramxv.singh@gmail.com>
2024-10-22 20:26:29 -0500

    refactor: change connection credentials to db

--- src/config/conndb.php
+++ src/config/conndb.php
@@ -7,7 +7,7 @@ class Database
 	{
 		if (self::$conn === null) {
 			try {
-				self::$conn = new PDO("mysql:host=localhost;dbname=proyecto_db", "d72024", "1234567");
+				self::$conn = new PDO("mysql:host=localhost;dbname=proyecto_db", "userdb", "passworddb");
 				self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			} catch (PDOException $e) {
 				
